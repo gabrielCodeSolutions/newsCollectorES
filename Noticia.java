783cb411b975dad7d4e9a4e8aeabee19b850a951
gabrielCodeSolutions <gabrielunitone@gmail.com>
2017-04-19 23:05:07 -0300

    Add files via upload

---
+++ Noticia.java
@@ -0,0 +1,20 @@
+
+public class Noticia {
+
+	String link;
+	String manchete;
+	
+	public Noticia(String link, String manchete) {
+		this.link = link;
+		this.manchete = manchete;
+	}
+	
+	public String getLink() {
+		return link;
+	}
+
+	public String getManchete() {
+		return manchete;
+	}
+
+}
