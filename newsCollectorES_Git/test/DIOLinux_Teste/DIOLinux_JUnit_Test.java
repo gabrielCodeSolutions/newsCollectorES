5b818c2cfe27ea0e6796ee4536329fdc4fe551e6
1151126435 <1151126435@C2a40brj504k3np.acad.unit.br>
2017-04-24 17:47:48 -0300

    1. Script de coleta e teste do site "OpenSource" iniciados. 2. Atualização do teste para site DIO Linux (novas matérias seleciona- das, do dia 24/04/2017).

--- newsCollectorES_Git/test/DIOLinux_Teste/DIOLinux_JUnit_Test.java
+++ newsCollectorES_Git/test/DIOLinux_Teste/DIOLinux_JUnit_Test.java
@@ -53,8 +53,8 @@ public class DIOLinux_JUnit_Test {
     @Test
     public void testeDIOCollector1(){
         
-        String manchete = "Ubuntu Gnome deixará de existir e se fundirá com o Ubuntu";
-        String link= "http://www.diolinux.com.br/2017/04/ubuntu-gnome-deixara-de-existir.html";
+        String manchete = "United - O tema para KDE Plasma que transforma a sua interface no Unity";
+        String link= "http://www.diolinux.com.br/2017/04/united-o-tema-unity-ubuntu-para-kde-plasma.html";
         Noticia noticia = new Noticia(link,manchete);
         // Teste de coleta da manchete e link
         System.out.println(mineradorDioLinux.getListaDeNoticias().get(4).getManchete());
@@ -64,8 +64,8 @@ public class DIOLinux_JUnit_Test {
     
     @Test
     public void testeDIOCollector2(){
-        String manchete = "Como criar e embutir legendas em vídeos usando Linux";
-        String link= "http://www.diolinux.com.br/2017/04/como-criar-e-embutir-legendas-em-videos-no-linux.html";
+        String manchete = "Como fazer o Google Chrome e o Firefox reconhecerem todos os Emojis";
+        String link= "http://www.diolinux.com.br/2017/04/como-fazer-o-google-chrome-reconhecerem-emojis.html";
         Noticia noticia = new Noticia(link,manchete);
         // Teste de coleta da manchete e link
         System.out.println(mineradorDioLinux.getListaDeNoticias().get(1).getManchete());
@@ -74,8 +74,8 @@ public class DIOLinux_JUnit_Test {
     
     @Test
     public void testeDIOCollector3(){
-        String manchete = "Anbox - O projeto que quer integrar Apps de Android nas distros Linux de Desktop";
-        String link= "http://www.diolinux.com.br/2017/04/anbox-app-android-linux-desktop.html";
+        String manchete = "Agricultores querem \"fazer root\" em tratores da John Deere";
+        String link= "http://www.diolinux.com.br/2017/04/agricultores-querem-fazer-root-em-tratores.html";
         Noticia noticia = new Noticia(link,manchete);
         // Teste de coleta da manchete e link
         System.out.println(mineradorDioLinux.getListaDeNoticias().get(2).getManchete());
@@ -84,8 +84,8 @@ public class DIOLinux_JUnit_Test {
     
     @Test
     public void testeDIOCollector4(){
-        String manchete = "ROSA Linux - Conheça um sistema operacional da Rússia!";
-        String link= "http://www.diolinux.com.br/2017/04/rosa-linux-sistema-operacional-russo.html";
+        String manchete = "Lançado Deepin 15.4 para provar que Linux não é difícil de uma vez por todas";
+        String link= "http://www.diolinux.com.br/2017/04/deepin-154-download-rapido.html";
         Noticia noticia = new Noticia(link,manchete);
         // Teste de coleta da manchete e link
         System.out.println(mineradorDioLinux.getListaDeNoticias().get(3).getManchete());
@@ -94,23 +94,38 @@ public class DIOLinux_JUnit_Test {
     
     @Test
     public void testeDIOCollector5(){
-    
+        String manchete = "Como criar e embutir legendas em vídeos usando Linux";
+        String link= "http://www.diolinux.com.br/2017/04/como-criar-e-embutir-legendas-em-videos-no-linux.html";
+        Noticia noticia = new Noticia(link,manchete);
+        // Teste de coleta da manchete e link
+        System.out.println(mineradorDioLinux.getListaDeNoticias().get(5).getManchete());
+        assertTrue(this.compararNoticia(noticia));
     }
     
     @Test
     public void testeDIOCollector6(){
-    
+        String manchete = "Anbox - O projeto que quer integrar Apps de Android nas distros Linux de Desktop";
+        String link= "http://www.diolinux.com.br/2017/04/anbox-app-android-linux-desktop.html";
+        Noticia noticia = new Noticia(link,manchete);
+        // Teste de coleta da manchete e link
+        System.out.println(mineradorDioLinux.getListaDeNoticias().get(6).getManchete());
+        assertTrue(this.compararNoticia(noticia));
     }
     
     @Test
     public void testeDIOCollector7(){
-    
+        String manchete = "Lançado Ubuntu 17.04 Zesty Zapus, confira as novidades faça o download!";
+        String link= "http://www.diolinux.com.br/2017/04/ubuntu-1704-download-torrent.html";
+        Noticia noticia = new Noticia(link,manchete);
+        // Teste de coleta da manchete e link
+        System.out.println(mineradorDioLinux.getListaDeNoticias().get(7).getManchete());
+        assertTrue(this.compararNoticia(noticia));
     }
     
     // Método utilizado para procurar uma dada noticia num ArrayList.
     public boolean compararNoticia(Noticia noticia){
         for (int i=0;i<=mineradorDioLinux.getListaDeNoticias().size();i++){
-            if (noticia.getManchete().equals(mineradorDioLinux.getListaDeNoticias().get(i).getManchete()) && noticia.getLink().equals(mineradorDioLinux.getListaDeNoticias().get(i).getLink())){
+            if (noticia.getManchete().equals(mineradorDioLinux.getListaDeNoticias().get(i).getManchete())|| noticia.getLink().equals(mineradorDioLinux.getListaDeNoticias().get(i).getLink())){
                 return true;
             } 
         }
